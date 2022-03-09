ab78019b7c2026224a0727c89c7e10945a4ce46a
eduardamirelly <mirelly.eduarda404@gmail.com>
2022-03-08 21:49:51 -0300

    solving bug about higher resolution screens

--- src/components/layouts/Section.js
+++ src/components/layouts/Section.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Section(props) {
   return (
-    <section className="w-full pb-16 pt-8 px-5 xl:px-12 flex flex-col items-center relative">
+    <section className="w-full pb-16 pt-8 px-5 flex flex-col items-center relative">
         {props.children}
     </section>
   )
